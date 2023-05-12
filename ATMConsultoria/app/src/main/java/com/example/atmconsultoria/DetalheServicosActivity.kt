c158a65b912c6a4cd8d244c3d686e7b04a983166
Rafael Pontes <rafael.spontes@hotmail.com>
2023-05-12 16:38:15 -0300

    curso_completo_de_kotlin_para_android
    Curso de Kotlin (IntelliJ IDEA / Android Studio)

---
+++ ATMConsultoria/app/src/main/java/com/example/atmconsultoria/DetalheServicosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.atmconsultoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheServicosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_servicos)
+    }
+}
---
+++ ATMConsultoria/app/src/main/java/com/example/atmconsultoria/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.atmconsultoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.btnClientes
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /*fun btnClientes(view: View) {
+        btnClientes.setOnClickListener {
+            Log.i("Botao", "Botão Pressionado!" )
+        }
+    }*/
+
+
+
+
+}
